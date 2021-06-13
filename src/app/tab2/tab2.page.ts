e391225b0ea40d1deb38d252e47637923c69f4bd
Juliano Aparecido Dezem Telles <juliano.telles@gmail.com>
2021-06-13 14:20:07 -0300

    Favotar Serie adicionado, com log

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  constructor(public alertController: AlertController) {}
 
-  constructor() {}
+  async exibirAlertaFavoritoSerie() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Deseja realmente favoritar a Série ?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Sim, favoritar',
+          handler: () => {
+            console.log('Confirm Okay');
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+  }
 }
